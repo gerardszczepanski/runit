a4320fc0d11cf2416f278564b156293338fdd93a
Gerard Szczepanski <gerardszczepanski@gmail.com>
2017-01-04 00:39:42 +0100

    FileResut -> display only file base name

--- runit-service/src/main/java/szczepanski/gerard/runit/service/result/FileResult.java
+++ runit-service/src/main/java/szczepanski/gerard/runit/service/result/FileResult.java
@@ -4,13 +4,14 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FilenameUtils;
+
 import javafx.scene.image.Image;
 import szczepanski.gerard.runit.common.exception.RunitRuntimeException;
 
 public class FileResult extends SearchResult {
 	
 	private final File file;
-	
 	
 	public static FileResult fromFile(File file) {
 		isFileValid(file);
@@ -24,7 +25,7 @@ public class FileResult extends SearchResult {
 	}
 	
 	private FileResult(File file) {
-		super(new SearchResultRepresentation(new Image(WebPageResult.class.getResourceAsStream("/images/search-results/fileResult.png")), file.getName()));
+		super(new SearchResultRepresentation(new Image(WebPageResult.class.getResourceAsStream("/images/search-results/fileResult.png")), FilenameUtils.getBaseName(file.getName())));
 		this.file = file;
 	}
 	
