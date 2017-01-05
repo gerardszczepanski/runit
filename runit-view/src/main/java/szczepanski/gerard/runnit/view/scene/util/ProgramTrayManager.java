1c3a0841f3955a003f3025265380f2e6e5caa7d2
Gerard Szczepanski <gerardszczepanski@gmail.com>
2017-01-05 23:46:07 +0100

    Refactor ProgramTrayManager

--- runit-view/src/main/java/szczepanski/gerard/runnit/view/scene/util/ProgramTrayManager.java
+++ runit-view/src/main/java/szczepanski/gerard/runnit/view/scene/util/ProgramTrayManager.java
@@ -1,12 +1,13 @@
 package szczepanski.gerard.runnit.view.scene.util;
 
+import java.awt.AWTException;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
+import java.awt.Toolkit;
 import java.awt.TrayIcon;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -27,48 +28,87 @@ public class ProgramTrayManager {
 
 	public static void installAsTrayIcon(Stage stage) {
 		try {
-			Platform.setImplicitExit(false);
-			java.awt.Toolkit.getDefaultToolkit();
-
-			if (!java.awt.SystemTray.isSupported()) {
-				LOG.debug("System tray is not supported. Application is being closed");
-				Platform.exit();
-			}
-
-			SystemTray tray = SystemTray.getSystemTray();
-			Dimension trayDim = tray.getTrayIconSize();
-			InputStream trayImageInputStream = ProgramTrayManager.class.getResourceAsStream(ProgramConfig.PROGRAM_ICON_PATH);
-			Image trayImage = ImageIO.read(trayImageInputStream);
-			trayImage = trayImage.getScaledInstance(trayDim.width, trayDim.height, Image.SCALE_SMOOTH);
-			TrayIcon trayIcon = new TrayIcon(trayImage, ProgramConfig.PROGRAM_TITLE);
-
-			trayIcon.addActionListener(event -> {
-				Platform.runLater(() -> {
-					LOG.debug("Show Program from Tray");
-					showStage(stage);
-				});
-			});
-
-			MenuItem exitItem = new MenuItem("Close");
-			exitItem.addActionListener(event -> {
-				LOG.debug("Close application");
-				Platform.exit();
-				tray.remove(trayIcon);
-			});
-
-			final PopupMenu popup = new PopupMenu();
-			popup.add(exitItem);
-			trayIcon.setPopupMenu(popup);
-
-			tray.add(trayIcon);
-		} catch (java.awt.AWTException | IOException e) {
+			installStageAsTray(stage);
+		} catch (java.awt.AWTException e) {
 			throw new RunitRuntimeException("Unable to install tray icon", e);
 		}
+	}
+	
+	private static void installStageAsTray(Stage stage) throws AWTException {
+		checkIfTraySupported();
+		initComponents();
+
+		SystemTray tray = SystemTray.getSystemTray();
+		TrayIcon trayIcon = createTrayIcon(tray, stage);
+		PopupMenu trayPopupMenu = crateTrayPopupMenu(tray, trayIcon);
+		trayIcon.setPopupMenu(trayPopupMenu);
+		tray.add(trayIcon);
+	}
+	
+	private static void checkIfTraySupported() {
+		if (!SystemTray.isSupported()) {
+			LOG.debug("System tray is not supported. Application is being closed");
+			Platform.exit();
+		}
+	}
+	
+	private static void initComponents() {
+		Platform.setImplicitExit(false);
+		Toolkit.getDefaultToolkit();
+	}
+	
+	private static TrayIcon createTrayIcon(SystemTray tray, Stage stage) {
+		Dimension trayDim = tray.getTrayIconSize();
+		Image trayImage = loadTrayImage(trayDim);
+		TrayIcon trayIcon = new TrayIcon(trayImage, ProgramConfig.PROGRAM_TITLE);
+
+		trayIcon.addActionListener(event -> {
+			Platform.runLater(() -> {
+				LOG.debug("Show Program from Tray");
+				showStage(stage);
+			});
+		});
+		
+		return trayIcon;
+	}
+	
+	private static Image loadTrayImage(Dimension trayDim) {
+		InputStream trayImageInputStream = ProgramTrayManager.class.getResourceAsStream(ProgramConfig.PROGRAM_ICON_PATH);
+		Image trayImage = tryToLoadTrayImageFromInputStream(trayImageInputStream);
+		trayImage = trayImage.getScaledInstance(trayDim.width, trayDim.height, Image.SCALE_SMOOTH);
+		return trayImage;
+	}
+	
+	private static Image tryToLoadTrayImageFromInputStream(InputStream in) {
+		try {
+			return ImageIO.read(in);
+		} catch (IOException e) {
+			throw new RunitRuntimeException("Unable to load TrayImage");
+		}
+	}
+	
+	private static PopupMenu crateTrayPopupMenu(SystemTray tray, TrayIcon trayIcon) {
+		MenuItem closeProgramItem = createCloseProgramItem(tray, trayIcon);
+
+		PopupMenu popupMenu = new PopupMenu();
+		popupMenu.add(closeProgramItem);
+		
+		return popupMenu;
+	}
+	
+	private static MenuItem createCloseProgramItem(SystemTray tray, TrayIcon trayIcon) {
+		MenuItem closeProgramItem = new MenuItem("Close");
+		closeProgramItem.addActionListener(event -> {
+			LOG.debug("Close application");
+			Platform.exit();
+			tray.remove(trayIcon);
+		});
+		
+		return closeProgramItem;
 	}
 
 	private static void showStage(Stage stage) {
 		stage.show();
 		stage.toFront();
 	}
-
 }
