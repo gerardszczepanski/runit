d3b6958fac59dbc892d0e7dcc68c1604de6785fc
Gerard Szczepanski <gerardszczepanski@gmail.com>
2017-01-27 23:35:31 +0100

    Fix SimpleSearchTermMatcher

--- runit-search-service/src/main/java/szczepanski/gerard/runit/search/service/service/impl/SimpleSearchTermMatcher.java
+++ runit-search-service/src/main/java/szczepanski/gerard/runit/search/service/service/impl/SimpleSearchTermMatcher.java
@@ -3,42 +3,46 @@ package szczepanski.gerard.runit.search.service.service.impl;
 import szczepanski.gerard.runit.service.service.SearchTermMatcher;
 
 /**
- * Simple searchTerm matching algorithm. It checks, if splitted by space
- * searchResutlName starts with searchTerm, both in lower case. If yes, then
- * matching is positive.
+ * Simple searchTerm matching algorithm. 
+ * It checks, if splitted by space searchResutlName starts with searchTerm, both in lower case.
+ * If yes, then matching is positive. 
  * 
- * Positive matching:
+ * Positive matching: 
  * 
- * searchTerm - google positive: google chrome positive: chrome google
+ * searchTerm - google
+ * positive: google chrome
+ * positive: chrome google
  * 
  * @author Gerard Szczepanski
  */
 public class SimpleSearchTermMatcher implements SearchTermMatcher {
-
+	
 	private static final String SPACE_REGEX = "\\s+";
-
+	
 	@Override
 	public boolean isMatch(String searchTerm, String searchResultName) {
-		String[] searchTermSplitted = searchTerm.split(SPACE_REGEX);
-		String[] searchResultNameSplitted = searchResultName.split(SPACE_REGEX);
-		return processSplitedSearchParts(searchTermSplitted, searchResultNameSplitted);
+		boolean isWholeSearchResultNameMatch = isResultNameStartsWithSearchTerm(searchTerm, searchResultName); 
+		
+		if (isWholeSearchResultNameMatch) {
+			return true;
+		} else {
+			String[] searchResultNameSplitted = searchResultName.split(SPACE_REGEX);
+			return processSplitedSearchResultName(searchTerm, searchResultNameSplitted);
+		}
 	}
-
-	private boolean processSplitedSearchParts(String[] searchTermSplitted, String[] searchResultNameSplitted) {
-		for (int i = 0; i < searchTermSplitted.length; i++) {
-			for (int j = 0; j < searchResultNameSplitted.length; j++) {
-
-				if (isSplittedResultNameStartsWithSplittedSearchTerm(searchTermSplitted[i], searchResultNameSplitted[j])) {
-					return true;
-				}
+	
+	private boolean processSplitedSearchResultName(String searchTerm, String[] searchResultNameSplitted) {
+		for (int i = 0; i < searchResultNameSplitted.length; i++) {
+			if (isResultNameStartsWithSearchTerm(searchTerm, searchResultNameSplitted[i])) {
+				return true;
 			}
 		}
-
+		
 		return false;
 	}
-
-	private boolean isSplittedResultNameStartsWithSplittedSearchTerm(String splitedSearchTermPart, String spliiedSearchResultNamePart) {
-		return spliiedSearchResultNamePart.toLowerCase().startsWith(splitedSearchTermPart.toLowerCase());
+	
+	private boolean isResultNameStartsWithSearchTerm(String searchTerm, String searchResultName) {
+		return searchResultName.toLowerCase().startsWith(searchTerm.toLowerCase());
 	}
 
 }
